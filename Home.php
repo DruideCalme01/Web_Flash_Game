89044e085e6bdd110d8256ee35e77e7026e9664e
Benjamin Chassier <benjamin.chassier@gmail.com>
2024-06-06 11:49:00 +0200

    ajout image nbr mystere

---
+++ Home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <header>
+            <h1>Web Flash Game</h1>
+        </header>
+        <main>
+            <section>
+            <!--memory
+            pierre feuille ciseux
+            morpion
+            labyrinthe-->
+                <img src="nbrmyster.jpg" alt="afficher image jeu nbr mystere">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </section> 
+            <section>
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </section>  
+            <section>
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </section>  
+            <section>
+                <img src="" alt="">
+                <img src="" alt="">
+            </section>  
+            <section>
+                <img src="" alt="">
+            </section>      
+        </main>
+    </body>
+</html>
